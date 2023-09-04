cb493d62423a58025aa85d1c317e766320804dcf
Simon-Honore <simon.honore6@gmail.com>
2023-09-04 11:01:36 +0200

    chore: config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			textColor: {
+				skin: {
+					primary: 'rgb(var(--color-text-primary) / <alpha-value>)',
+					secondary: 'rgb(var(--color-text-secondary) / <alpha-value>)',
+				},
+			},
+			colors: {
+				background: 'rgb(var(--color-background) / <alpha-value>)',
+				paper: 'rgb(var(--color-paper) / <alpha-value>)',
+
+				primary: 'rgb(var(--color-primary) / <alpha-value>)',
+				secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+			},
+		},
+	},
+	plugins: [],
+};
