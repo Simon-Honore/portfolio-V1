0df988c917d2c49e007afab921153fabf761c2c6
Simon-Honore <simon.honore6@gmail.com>
2023-09-05 15:08:12 +0200

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import { ContactLink } from './atom/ContactLink';
+
 export const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className='mt-20 border-t-4 p-10 flex flex-col items-center gap-6'>
+			<ContactLink />
+			<div className='flex flex-col gap-2 text-sm'>
+				<p>Designed and built by Simon Honoré</p>
+				<p>©Copyright 2023 - Simon Honoré</p>
+			</div>
+		</footer>
+	);
 };
