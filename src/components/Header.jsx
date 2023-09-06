456abdda61be298cecaa4de763e89a330c33c0cd
Simon-Honore <simon.honore6@gmail.com>
2023-09-06 10:51:45 +0200

    refactor: add link on name title into header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FULL_NAME } from '../lib/config';
 import { ToggleThemeButton } from './ToggleThemeButton';
 import { NavLink } from './atom/NavLinks';
@@ -5,9 +6,12 @@ import { NavLink } from './atom/NavLinks';
 export const Header = () => {
 	return (
 		<header className='flex py-6'>
-			<span className='bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-extrabold text-transparent drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)]'>
+			<Link
+				href={'/'}
+				className='bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-extrabold text-transparent drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)]'
+			>
 				{FULL_NAME}
-			</span>
+			</Link>
 			<NavLink />
 			<ToggleThemeButton />
 		</header>
